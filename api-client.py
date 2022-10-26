63c93dd63d87f7cd36965382418bd83d62bece27
Johannes <116494753+JohannesHoivik@users.noreply.github.com>
2022-10-26 14:53:38 +0200

    rettelse av feil ved lagring av filer

---
+++ api-client.py
@@ -0,0 +1,9 @@
+def subscribe_to_api():
+    g
+def unsubscribe_to_api():
+    g
+def receive_request():
+    g
+
+
+
--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+from entur-interface.innstillinger import Innstillinger
+import innstillinger
+
+def main():
+    x = Innstillinger()
+    Innstillinger()
+    x.stoppested()
+
+    print("hello world")
+
+if __name__ == "__main__":
+    main()
