2d8fdc62adfbf411debafdf61b922d294491b058
Johannes <116494753+JohannesHoivik@users.noreply.github.com>
2022-10-26 14:51:50 +0200

    test av liten endring

---
+++ innstillinger.py
@@ -0,0 +1,23 @@
+
+def les_fil():
+    g
+def stoppested():
+    g
+def valg_av_linje(): #velge hvilket stoppested
+    g
+def mengden_avganger(): #om du vil vise de 20 neste avgangene, eller mer/mindre
+    g
+
+class Innstillinger():
+    stoppested = None
+    y = None
+    def __init__(self, file) -> None:
+        self.stoppested = 
+        pass
+    def stoppested(self):
+        return self.stoppested
+        
+    #lese filen, hente info, lagre i variabler
+
+def test(x):
+    return x
